96ffde6b40d428b67b9d218f291a15a351a59825
AlexandreMelo <alemelo18lds@gmail.com>
2024-06-23 23:21:02 -0300

    Commit desáfio

---
+++ app.js
@@ -0,0 +1,26 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = "Hora do Desáfio";
+
+function consoleClique(){
+    console.log("O botão foi clicado");
+} 
+function alertClique(){
+    alert("Eu amo JS");
+} 
+function promptClique(){
+    let cidade = prompt("Digite o nome de uma cidade");
+    alert(`Estive em ${cidade} e lembrei de você`);
+} 
+function somaClique(){
+
+    let digitoUm = prompt("Digite um número inteiro");
+    let digitoDois = prompt("Digite outro número, irei somá-los")
+
+//parseInt converte as Strings em inteiros, pois o prompt retorna somente valores em formato de String.
+    let numeroUm = parseInt(digitoUm);
+    let numeroDois = parseInt(digitoDois);
+
+    let resultadoSoma = numeroUm + numeroDois;
+    alert(resultadoSoma);
+}
+
